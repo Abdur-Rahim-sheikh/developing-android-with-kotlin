bf16cbea367fd252f1c83a1ec9c0bcfe59b3f1e7
Abdur Rahim sheikh <abdur6552@gmail.com>
2021-12-13 15:57:00 +0600

    optimized findview call and intorduced vectordrawable

--- diceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +21,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6)+1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+//        val diceImage: ImageView = findViewById(R.id.dice_image)
 
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
